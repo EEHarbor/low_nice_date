9c90e2e376522cc37a0c3b7a26f745d79aa7aefa
Lodewijk Schutte <lodewijk@gmail.com>
2013-05-31 12:12:04 +0200

    v220

--- low_nice_date/pi.low_nice_date.php
+++ low_nice_date/pi.low_nice_date.php
@@ -2,149 +2,140 @@
 
 $plugin_info = array(
 	'pi_name'			=> 'Low Nice Date',
-	'pi_version'		=> '2.1',
+	'pi_version'		=> '2.2.0',
 	'pi_author'			=> 'Lodewijk Schutte ~ Low',
-	'pi_author_url'		=> 'http://loweblog.com/freelance/article/ee-nice-date-plugin/',
+	'pi_author_url'		=> 'http://gotolow.com/addons/low-nice-date',
 	'pi_description'	=> 'Displays a nice date.',
-	'pi_usage'			=> Low_nice_date::usage()
+	'pi_usage'			=> 'See http://gotolow.com/addons/low-nice-date for more info.'
 );
 
 /**
-* Low Nice Date Plugin class
-*
-* @package			low-nice_date-ee2_addon
-* @version			2.0
-* @author			Lodewijk Schutte ~ Low <low@loweblog.com>
-* @link				http://loweblog.com/freelance/article/ee-nice-date-plugin/
-* @license			http://creativecommons.org/licenses/by-sa/3.0/
-*/
+ * < EE 2.6.0 backward compat
+ */
+if ( ! function_exists('ee'))
+{
+	function ee()
+	{
+		static $EE;
+		if ( ! $EE) $EE = get_instance();
+		return $EE;
+	}
+}
+
+/**
+ * Low Nice Date Plugin class
+ *
+ * @package        low_nice_date
+ * @author         Lodewijk Schutte <hi@gotolow.com>
+ * @link           http://gotolow.com/addons/low-nice-date
+ * @license        http://creativecommons.org/licenses/by-sa/3.0/
+ */
 class Low_nice_date {
 
-	/**
-	* Plugin return data
-	*
-	* @var	string
-	*/
-	var $return_data;
-
+	// --------------------------------------------------------------------
+	// PROPERTIES
 	// --------------------------------------------------------------------
 
 	/**
-	* PHP4 Constructor
-	*
-	* @see	__construct()
-	*/
-	function Low_nice_date($date = '', $format = '', $loc = FALSE)
+	 * Plugin return data
+	 *
+	 * @var        string
+	 */
+	public $return_data;
+
+	// --------------------------------------------------------------------
+	// METHODS
+	// --------------------------------------------------------------------
+
+	/**
+	 * Constructor
+	 *
+ 	 * @access     public
+	 * @return     string
+	 */
+	public function __construct()
 	{
-		$this->__construct($date, $format, $loc);
+		// -------------------------------------------
+		// Get parameters
+		// -------------------------------------------
+
+		$date   = ee()->TMPL->fetch_param('date', time());
+		$format = ee()->TMPL->fetch_param('format');
+		$loc    = (ee()->TMPL->fetch_param('localize') == 'yes');
+
+		// -------------------------------------------
+		// Convert date to timestamp, force GMT/UTC date
+		// -------------------------------------------
+
+		$time = $this->_stamp($date);
+
+		// -------------------------------------------
+		// Format timestamp
+		// -------------------------------------------
+
+		$this->return_data = ($time && $format) ? ee()->localize->decode_date($format, $time, $loc) : $date;
 	}
 
 	// --------------------------------------------------------------------
 
 	/**
-	* PHP5 Constructor
-	*
-	* @param	string	$date
-	* @param	string	$format
-	* @param	bool	$loc
-	* @return	string
-	*/
-	function __construct($date = '', $format = '', $loc = FALSE)
+	 * Calculate difference in years, months and days in given date range
+	 *
+	 * @access     public
+	 * @return     string
+	 */
+	public function range()
 	{
-		/** -------------------------------------
-		/**  Get global instance
-		/** -------------------------------------*/
+		// -------------------------------------------
+		// Get From and To values
+		// -------------------------------------------
 
-		$this->EE =& get_instance();
+		$data = array(
+			'from' => $this->_stamp(ee()->TMPL->fetch_param('from', time())),
+			'to'   => $this->_stamp(ee()->TMPL->fetch_param('to', time()))
+		);
 
-		/** -------------------------------------
-		/**  Get parameters
-		/** -------------------------------------*/
-
-		if ($date == '')
-		{
-			$date = $this->EE->TMPL->fetch_param('date', '');
-		}
-		
-		if ($format == '')
-		{
-			$format = $this->EE->TMPL->fetch_param('format', '');
-		}
-
-		if ($loc !== FALSE)
-		{
-			$loc = ($this->EE->TMPL->fetch_param('localize') === 'yes') ? TRUE : FALSE;
-		}
-
-		/** -------------------------------------
-		/**  Convert date to timestamp, force GMT/UTC date
-		/** -------------------------------------*/
-
-		$time = strtotime($date.' UTC');
-		
-		/** -------------------------------------
-		/**  Format timestamp
-		/** -------------------------------------*/
-
-		$this->return_data = ($time && $format != '') ? $this->EE->localize->decode_date($format, $time, $loc) : $date;
-		
-		return $this->return_data;
-	}
-
-	// --------------------------------------------------------------------
-
-	/**
-	* Calculate difference in years, months and days in given date range
-	*
-	* @return	string
-	*/	
-	function range()
-	{
-		$data = array();
-
-		$data['from'] = $this->EE->TMPL->fetch_param('from');
-		$data['to']   = $this->EE->TMPL->fetch_param('to');
+		// -------------------------------------------
+		// Get absolute difference between the two
+		// -------------------------------------------
 
 		$diff = abs($data['to'] - $data['from']);
 
+		// -------------------------------------------
+		// Get number of years, months and days for this difference
+		// -------------------------------------------
+
 		$data['years']  = floor($diff / (365*60*60*24));
 		$data['months'] = floor(($diff - $data['years'] * 365*60*60*24) / (30*60*60*24));
-		$data['days']   = floor(($diff - $data['years'] * 365*60*60*24 - $data['months']*30*60*60*24)/ (60*60*24));
+		$data['days']   = floor(($diff - $data['years'] * 365*60*60*24 - $data['months']*30*60*60*24) / (60*60*24));
 
-		return $this->EE->TMPL->parse_variables($this->EE->TMPL->tagdata, array($data));
+		// -------------------------------------------
+		// Parse the template
+		// -------------------------------------------
+
+		return ee()->TMPL->parse_variables_row(ee()->TMPL->tagdata, $data);
 	}
 
 	// --------------------------------------------------------------------
-	
+
 	/**
-	* Plugin Usage
-	*
-	* @return	string
-	*/    
-	function usage()
+	 * Get timestamp for date
+	 *
+	 * @access     private
+	 * @param      string
+	 * @param      bool
+	 * @return     int
+	 */
+	private function _stamp($str, $utc = TRUE)
 	{
-		ob_start(); 
-		?>
-			{exp:low_nice_date date="2007-05-20" format="%F %j%S, %Y" localize="yes"}
+		if ( ! is_numeric($str))
+		{
+			if ($utc) $str .= ' UTC';
+			$str = strtotime($str);
+		}
 
-			{exp:low_nice_date date="{segment_3}-{segment_4}-01" format="%F %Y"}
-
-			{exp:low_nice_date:range from="{date_1}" to="{date_2}"}
-				{if days > 0}
-					This takes longer than a day
-				{if:else}
-					Range in the same day
-				{/if}
-			{/exp:low_nice_date:range}
-		<?php
-		$buffer = ob_get_contents();
-  
-		ob_end_clean(); 
-
-		return $buffer;
+		return (int) $str;
 	}
-
-	// --------------------------------------------------------------------
 
 }
 // END CLASS
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Low Nice Date for ExpressionEngine 2
+
+See [gotolow.com](http://gotolow.com/addons/low-nice-date) for more info.
